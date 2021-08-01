530951e22e186a1f540414c83375454fac35f9cc
rahul020800 <mauryarahda18ce@student.mes.ac.in>
2021-08-01 07:47:59 +0530

    Updates

---
+++ precache-manifest.8327582eaa65e4ba9eb4abde2bb1090f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849dee1db8710e6f68e88f03bd623e6f",
+    "url": "/Global2/index.html"
+  },
+  {
+    "revision": "8fdbe3799f5ca05ccb3a",
+    "url": "/Global2/static/css/main.0ff0dcd2.chunk.css"
+  },
+  {
+    "revision": "0016d5366d0cc3f98095",
+    "url": "/Global2/static/js/2.7d203c97.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Global2/static/js/2.7d203c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdbe3799f5ca05ccb3a",
+    "url": "/Global2/static/js/main.46f525c6.chunk.js"
+  },
+  {
+    "revision": "62dab7f3253875209cad",
+    "url": "/Global2/static/js/runtime-main.12fb887d.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Global2/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
